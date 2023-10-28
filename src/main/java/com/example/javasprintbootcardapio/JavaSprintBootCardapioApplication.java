9ac6bbf1ab13cf02b6ca60d88d9c915b7bbc8139
lucassimaog6 <22101918@aluno.cotemig.com.br>
2023-10-28 09:15:44 -0300

    initial setup

---
+++ src/main/java/com/example/javasprintbootcardapio/JavaSprintBootCardapioApplication.java
@@ -0,0 +1,13 @@
+package com.example.javasprintbootcardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprintBootCardapioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSprintBootCardapioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javasprintbootcardapio/JavaSprintBootCardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javasprintbootcardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprintBootCardapioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
