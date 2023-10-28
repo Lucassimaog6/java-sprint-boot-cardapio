0eca55cb3183657cbcef6f23975820ad7718a6fa
lucassimaog6 <22101918@aluno.cotemig.com.br>
2023-10-28 10:13:36 -0300

    Improve Food Model

--- src/main/java/com/example/javasprintbootcardapio/models/Food.java
+++ src/main/java/com/example/javasprintbootcardapio/models/Food.java
@@ -1,13 +1,28 @@
 package com.example.javasprintbootcardapio.models;
 
+import com.example.javasprintbootcardapio.controller.FoodRequest;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "food")
 @Entity(name = "food")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String image;
     private Integer price;
+
+    public Food(FoodRequest foodRequest) {
+        this.image = foodRequest.image();
+        this.title = foodRequest.title();
+        this.price = foodRequest.price();
+    }
 }
