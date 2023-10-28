cd27b12543e1f7d6532af0417767ec02d8252ac4
lucassimaog6 <22101918@aluno.cotemig.com.br>
2023-10-28 13:00:06 -0300

    add repository annotation

--- src/main/java/com/example/javasprintbootcardapio/repositories/FoodRepository.java
+++ src/main/java/com/example/javasprintbootcardapio/repositories/FoodRepository.java
@@ -2,6 +2,8 @@ package com.example.javasprintbootcardapio.repositories;
 
 import com.example.javasprintbootcardapio.models.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
 }
